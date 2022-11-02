874c08949fccad5f404a1a151117d222726b0634
fmehmood12345 <mehmood.far@gmail.com>
2022-11-02 12:11:34 +0000

    Everything works except when I delete item client, the CSV file adds a gap between each row and also printed the list which isn't exactly like a table more like a line of all content of the CSV file

--- banking_application.py
+++ banking_application.py
@@ -2,33 +2,36 @@ import csv
 import database
 import  clients
 from csv import writer
-
 #class for banking application
 class Banking_Application:
-
     def __init__(self):
         with open("database/clients_details.csv","r") as x:
             reader = csv.reader(x)
             for i in reader:
                 print(i)
 
-#Adding Clients: appending client details to csv file
 filename = 'database/clients_details.csv'
 
-# with open(filename, 'a',newline='') as file_object:
-#     writer_object = writer(file_object)
-#     client1 = clients.client_details('Ms', 'Skyler', 'Harrinson', 'they', '2/23/1960', 'Research and Development',
-#                                      '4562', '100')
-#     client1_list = []
-#     for value in client1.__str__().split(','):
-#         client1_list.append(value)
-#     writer_object.writerow(client1_list)
-#
-# file_object.close()
+#Adding Clients: appending client details to csv file
+with open(filename, 'a',newline='') as file_object:
+    writer_object = writer(file_object)
+    client1 = clients.client_details('Ms', 'Skyler', 'Harrinson', 'they', '2/23/1960', 'Research and Development',
+                                     '4562', '100')
+    client1_list = []
+    for value in client1.__str__().split(','):
+        client1_list.append(value)
+    writer_object.writerow(client1_list)
 
-#Deleting Clients from CSV file
+file_object.close()
+
+#Proof of Adding Clients
+with open(filename) as file_object:
+     content = file_object.read()
+print(content)
+
+#Deleting Cliets form CSV file
 lines = list()
-client_name = input("Enter the firstname of the client you want to delete")
+client_name = input("Enter the firstname of the client you want to delete ")
 with open(filename, 'r') as readFile:
     reader = csv.reader(readFile)
     for row in reader:
@@ -37,17 +40,10 @@ with open(filename, 'r') as readFile:
             if field == client_name:
                 lines.remove(row)
 
-
-                for i in lines:
-                    print(i)
-
+#Writing the CSV file into a list
 with open(filename, 'w') as writeFile:
     writer = csv.writer(writeFile)
     writer.writerows(lines)
 
-#Printing entire CSV file
-with open('database/clients_details.csv') as file_object:
-    content = file_object.read()
-print(content)
-
+print(lines)
 
