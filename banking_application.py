0162663c8de511e1450a34913633d1d28d406dd0
fmehmood12345 <mehmood.far@gmail.com>
2022-11-03 14:39:01 +0000

    Changing structure of code. Added a distionary into clients.py

--- banking_application.py
+++ banking_application.py
@@ -1,51 +1,51 @@
-import csv
-import database
-import  clients
-from csv import writer
-#class for banking application
-class Banking_Application:
-    def __init__(self):
-        with open("database/clients_details.csv","r") as x:
-            reader = csv.reader(x)
-            for i in reader:
-                print(i)
-
-filename = 'database/clients_details.csv'
-
-#Adding Clients: appending client details to csv file
-with open(filename, 'a',newline='') as file_object:
-    writer_object = writer(file_object)
-    client1 = clients.client_details('Ms', 'Skyler', 'Harrinson', 'they', '2/23/1960', 'Research and Development',
-                                     '4562', '100')
-    client1_list = []
-    for value in client1.__str__().split(','):
-        client1_list.append(value)
-    writer_object.writerow(client1_list)
-
-file_object.close()
-
-#Proof of Adding Clients
-with open(filename) as file_object:
-     content = file_object.read()
-print(content)
-
-#Deleting Cliets from CSV file
-lines = list()
-client_name = input("Enter the firstname of the client you want to delete ")
-with open(filename, 'r') as readFile:
-    reader = csv.reader(readFile)
-    for row in reader:
-        lines.append(row)
-        for field in row:
-            if field == client_name:
-                lines.remove(row)
-    for i in lines:
-        print(i)
-
-#Writing the CSV file into a list
-with open(filename, 'w') as writeFile:
-    writer = csv.writer(writeFile)
-    writer.writerows(lines)
-# print(lines)
-
-
+# import csv
+# import database
+# import  clients
+# from csv import writer
+# #class for banking application
+# class Banking_Application:
+#     def __init__(self):
+#         with open("database/clients_details.csv","r") as x:
+#             reader = csv.reader(x)
+#             for i in reader:
+#                 print(i)
+#
+# filename = 'database/clients_details.csv'
+#
+# #Adding Clients: appending client details to csv file
+# with open(filename, 'a',newline='') as file_object:
+#     writer_object = writer(file_object)
+#     client1 = clients.client_details('Ms', 'Skyler', 'Harrinson', 'they', '2/23/1960', 'Research and Development',
+#                                      '4562', '100')
+#     client1_list = []
+#     for value in client1.__str__().split(','):
+#         client1_list.append(value)
+#     writer_object.writerow(client1_list)
+#
+# file_object.close()
+#
+# #Proof of Adding Clients
+# with open(filename) as file_object:
+#      content = file_object.read()
+# print(content)
+#
+# #Deleting Cliets from CSV file
+# lines = list()
+# client_name = input("Enter the firstname of the client you want to delete ")
+# with open(filename, 'r') as readFile:
+#     reader = csv.reader(readFile)
+#     for row in reader:
+#         lines.append(row)
+#         for field in row:
+#             if field == client_name:
+#                 lines.remove(row)
+#     for i in lines:
+#         print(i)
+#
+# #Writing the CSV file into a list
+# with open(filename, 'w') as writeFile:
+#     writer = csv.writer(writeFile)
+#     writer.writerows(lines)
+# # print(lines)
+#
+#
--- clients.py
+++ clients.py
@@ -13,17 +13,30 @@ class client_details:
         self.account_balance = account_balance
         self.overdraft_limit = overdraft_limit
 
-
-    #method explaining how data should be ordered
-    def order(self):
-        return (
-            f"{self.title},{self.first_name},{self.last_name},{self.preferred_pronouns},{self.date_of_birth},{self.occupation},{self.account_balance},{self.overdraft_limit}")
-
+    # #method explaining how data should be ordered
+    # def order(self):
+    #     return (
+    #         f"{self.title},{self.first_name},{self.last_name},{self.preferred_pronouns},{self.date_of_birth},{self.occupation},{self.account_balance},{self.overdraft_limit}")
+    #
     def __str__(self):
-        return (
-            f'{self.title},{self.first_name},{self.last_name},{self.preferred_pronouns},{self.date_of_birth},{self.occupation},{self.account_balance},{self.overdraft_limit}')
+         return (
+             f'{self.title},{self.first_name},{self.last_name},{self.preferred_pronouns},{self.date_of_birth},{self.occupation},{self.account_balance},{self.overdraft_limit}')
 
     def __repr__(self):
         return (
             f"{self.title},{self.first_name},{self.last_name},{self.preferred_pronouns},{self.date_of_birth},{self.occupation},{self.account_balance},{self.overdraft_limit}")
 
+    def changing_title(self,title, first_name, last_name):
+
+client = {
+        "title": "Mr",
+        "first_name" : "Mike",
+        "last_name": "Smith",
+        "preferred_pronouns": "he/him",
+        "date_of_birth": "15/12/1990",
+        "occupation": "Software Engineer",
+        "account_balance": "158972",
+        "overdraft_limit": "1000"
+    }
+c = client_details(**client)
+print(c)
