2a007d30cb92469934040401d0c2074b88b4a248
fmehmood12345 <mehmood.far@gmail.com>
2022-11-05 21:27:06 +0000

    Made more code more logical by getting rid of unnecessary variables and showing that changes have been made. Also adding code which updates attributes

--- clients.py
+++ clients.py
@@ -20,48 +20,46 @@ class client_details:
     #
     def __str__(self):
          return (
-             f'{self.title},{self.first_name},{self.last_name},{self.preferred_pronouns},{self.date_of_birth},{self.occupation},{self.account_balance},{self.overdraft_limit}')
+             f'Title: {self.title}\n'
+             f'Firstname: {self.first_name}\n'
+             f'Lastname: {self.last_name}\n'
+             f'Preferred Pronoun: {self.preferred_pronouns}\n'
+             f'Date of Birth: {self.date_of_birth}\n'
+             f'Occupation: {self.occupation}\n'
+             f'Account Balance: £{self.account_balance}\n'
+             f'Overdraft Limit: £{self.overdraft_limit}')
 
     def __repr__(self):
         return (
             f"{self.title},{self.first_name},{self.last_name},{self.preferred_pronouns},{self.date_of_birth},{self.occupation},{self.account_balance},{self.overdraft_limit}")
 
     def adding_in_account_balance(self,add_balance):
-        old_account_balance = self.account_balance
-        new_balance =  int(add_balance) + int(old_account_balance)
-        return  new_balance
+        new_balance =  int(add_balance) + int(self.account_balance)
+        self.account_balance=new_balance
+
 
     def removing_in_account_balance(self,removing_balance):
-        old_account_balance = self.account_balance
-        new_balance =  int(old_account_balance)-int(removing_balance)
+        new_balance =  int(self.account_balance)-int(removing_balance)
+        self.account_balance = new_balance
         if removing_balance > (int(old_account_balance)+ int(self.overdraft_limit)):
             new_balance = 0- (removing_balance + 5) # HAVE TO CHECK
-        return new_balance
+            self.account_balance = new_balance
 
     def changing_title(self,new_title):
-        old_title = self.title
-        updated_title=new_title
-        return updated_title
+        self.title = new_title
 
     def changing_first_name(self,new_first_name):
-        old_first_name = self.first_name
-        updated_first_name = new_first_name
-        return updated_first_name
+        self.first_name = new_first_name
     def changing_last_name(self,new_last_name):
-        old_last_name = self.last_name
-        updated_last_name = new_last_name
-        return updated_last_name
+        self.last_name = new_last_name
 
     def changing_preferred_pronoun(self,new_preferred_pronoun):
-        old_preferred_pronoun = self.preferred_pronouns
-        updated_preferred_pronoun = new_preferred_pronoun
-        return updated_preferred_pronoun
+        self.preferred_pronouns=new_preferred_pronoun
 
     def changing_occupation(self,new_occupation):
-        old_occupation = self.occupation
-        updated_occupation = new_occupation
-        return updated_occupation
+        self.occupation = new_occupation
 
+#One Client
 client = {
         "title": "Mr",
         "first_name" : "Mike",
@@ -78,30 +76,15 @@ c = client_details(**client)
 #Client before changes made
 print(c)
 
-# #Adding balance
-# c.account_balance=(c.adding_in_account_balance(add_balance=100))
-# print(c.account_balance)
+print(" ")
 
-#removing balance
-c.account_balance=(c.removing_in_account_balance(removing_balance=1100))
-print(c.account_balance)
+#Changes made to clients
+c.changing_preferred_pronoun("she/her")
+c.changing_last_name("Kim")
+c.changing_title("Miss")
+c.changing_occupation("Doctor")
+c.changing_first_name("Sarah")
 
-#changing the title
-c.title=(c.changing_title("Mrs"))
-print(c.title)
-
-#changing the firstname
-c.first_name=(c.changing_first_name("Sarah"))
-print(c.first_name)
-
-c.last_name=(c.changing_last_name("Dott"))
-print(c.last_name)
-
-c.preferred_pronouns=(c.changing_preferred_pronoun("she/her"))
-print(c.preferred_pronouns)
-
-c.occupation=(c.changing_occupation("Teacher"))
-print(c.occupation)
-
-#Check to see if the dictionary is actually updated
+#Clients after changes made
 print(c)
+
